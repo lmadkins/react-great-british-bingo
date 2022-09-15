d9d487e594cd26fbe28d6b670d11510fe8aa8acf
lmadkins <louise.m.adkins@gmail.com>
2022-09-15 15:39:08 -0700

    Mapped array items, shuffle functionality. Some refactoring needed.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-// import React, { useState } from 'react';
+// import React, { useState, useEffect } from 'react';
 // import { Routes, Route, Link } from "react-router-dom"
 // import GamePage from './components/GamePage';
 // import Navbar from './components/Navbar';
 // import GameIntro from './components/GameIntro'
 import jsonArr from './data/promptList.js'
-import winningCombos from'./data/winningCombos'
+// import winningCombos from'./data/winningCombos'
 
 function App() {
-// console.log(jsonArr)
-// console.log(winningCombos)
+
+  // const [restart, setRestart] = useState()
 
 // PSEUDOCODE
 // initial game state, all squares are marked: false
@@ -21,6 +21,30 @@ function App() {
   // extract first 25 from that new shuffled array
   // render/map grid squares w/ those prompts (0-24)
     // exclude middle 'free' square (square 13)
+  
+let row1 = []
+let row2 = []
+let row3 = []
+let row4 = []
+let row5 = []
+let shuffledArr = []
+
+function shuffleArr() {
+  shuffledArr = jsonArr.sort(() => Math.random() - 0.5)
+}
+
+function renderNew() {
+  shuffleArr()
+  let row1 = shuffledArr.slice(0,5)
+  // console.log(row1)
+  // console.log('renderClick has run')
+  // return shuffledArr
+}
+
+const handleRestartClick = () => {
+  // console.log('handleRestartClick has run')
+  renderNew()
+}
 
 // restart game:
 // clear previous round:
@@ -47,59 +71,49 @@ function App() {
       </nav>
       <main>
         {/* Game intro page */}
-      <h1>Welcome to the Great British Bakeoff Bingo</h1>
-      <p><strong>How to Play:</strong>
-      <br></br>
-        While watching, keep an eye out for when something happens matching one of your card prompts. When it does, click or tap to mark it off. When you get 3 in a row (horizontally or vertically), you win! </p>
-      <button>Ready?
-        On your mark, get set, PLAY!</button>
+      <div>
+        <h1>Welcome to the Great British Bakeoff Bingo</h1>
+        <p><strong>How to Play:</strong>
+        <br></br>
+          While watching, keep an eye out for when something happens matching one of your card prompts. When it does, click or tap to mark it off. When you get 3 in a row (horizontally or vertically), you win! </p>
+          <button>
+          Ready?
+          On your mark, get set, PLAY!</button>
+      </div>  
         {/* <Routes>
             <Route path="/" element={<GameIntro />}/>
             <Route path="/play" element={<GamePage />}/>
         </Routes> */}
 
         {/* Game page */}
-        <button>Restart Game</button>
+        <button
+          onClick={handleRestartClick}
+        >Restart Game</button>
+      
         <table role='grid'>
-          <body>
-            <tr role='row' className="row1" >
-              <td role='gridcell' className='cell0'>Cell 0</td>
-              <td role='gridcell'className='cell1'>Cell 1</td>
-              <td role='gridcell' className='cell2'>Cell 2</td>
-              <td role='gridcell' className='cell3'>Cell 3</td>
-              <td role='gridcell' className='cell4'>Cell 4</td>
-              {/* <!--- 4 more grid cells ---> */}
-            </tr>
-            <tr role='row' className="row2" >
-              <td role='gridcell' className='cell5'>Cell 5</td>
-              <td role='gridcell' className='cell6'>Cell 6</td>
-              <td role='gridcell' className='cell7'>Cell 7</td>
-              <td role='gridcell' className='cell8'>Cell 8</td>
-              <td role='gridcell' className='cell9'>Cell 9</td>
-            </tr>
-            <tr role='row' className="row3" >
-            <td role='gridcell' className='cell10'>Cell 10</td>
-              <td role='gridcell' className='cell11'>Cell 11</td>
-              <td role='gridcell' className='free cell12'>FREE</td>
-              <td role='gridcell' className='cell13'>Cell 13</td>
-              <td role='gridcell' className='cell14'>Cell 14</td>
-            </tr>
-            <tr role='row' className="row4" >
-              <td role='gridcell' className='cell15'>Cell 15</td>
-              <td role='gridcell' className='cell16'>Cell 16</td>
-              <td role='gridcell' className='cell17'>Cell 17</td>
-              <td role='gridcell' className='cell18'>Cell 18</td>
-              <td role='gridcell' className='cell19'>Cell 19</td>
+          <tr role='row' className="row1" >
+            {jsonArr.slice(0,5).map(prompt => {return <td role='gridcell' text={prompt.prompt} key={prompt.id}>{prompt.prompt}</td> })}
+          </tr>
 
-            </tr>
-            <tr role='row' className="row5" >
-              <td role='gridcell'className='cell20'>Cell 20</td>
-              <td role='gridcell' className='cell21'>Cell 21</td>
-              <td role='gridcell' className='cell22'>Cell 22</td>
-              <td role='gridcell' className='cell23'>Cell 23</td>
-              <td role='gridcell' className='cell24'>Cell 24</td>
-            </tr>
-          </body>
+          <tr role='row' className="row2" >
+            {jsonArr.slice(6,11).map(prompt => {return <td role='gridcell' text={prompt.prompt} key={prompt.id}>{prompt.prompt}</td> })}
+          </tr>
+
+          <tr role='row' className="row3" >
+            {jsonArr.slice(12,14).map(prompt => {return <td role='gridcell' text={prompt.prompt} key={prompt.id}>{prompt.prompt}</td> })}
+
+            <td role='gridcell' className='free cell12'>FREE</td>
+            
+            {jsonArr.slice(15,17).map(prompt => {return <td role='gridcell' text={prompt.prompt} key={prompt.id}>{prompt.prompt}</td> })}
+          </tr>
+
+          <tr role='row' className="row4" >
+              {jsonArr.slice(18,23).map(prompt => {return <td role='gridcell' text={prompt.prompt} key={prompt.id}>{prompt.prompt}</td> })}
+          </tr>
+
+          <tr role='row' className="row5" >
+            {jsonArr.slice(24,29).map(prompt => {return <td role='gridcell' text={prompt.prompt} key={prompt.id}>{prompt.prompt}</td> })}
+          </tr>
         </table>
       </main>
     </>
--- src/data/promptList.js
+++ src/data/promptList.js
@@ -1,104 +1,138 @@
 const jsonArr = [
 {
+  "id": 1,
   "prompt": "A bake collapses/breaks",
 },
 {
+  "id": 2,
   "prompt": "Baker helps another baker",
 },
 {
+  "id": 3,
   "prompt": "Bakers haven't heard of something before",
 },
 {
+  "id": 4,
   "prompt": "Dry",
 },
 {
+  "id": 5,
   "prompt": "Even layering",
 },
 {
+  "id": 6,
   "prompt":"Flavors are not coming through",
 },
 {
+  "id": 7,
   "prompt": "Flavors are too strong",
 },
 {
+  "id": 8,
   "prompt": "Good/beautiful sponge",
 },
 {
+  "id": 9,
   "prompt": "Handshake from Paul",
 },
 {
+  "id": 10,
   "prompt": "Hosts wearing costumes",
 },
 {
+  "id": 11,
   "prompt": "Hot day in the tent",
 },
 {
+  "id": 12,
   "prompt": "I've never made this before",
 },
 {
+  "id": 13,
   "prompt": "Innuendo",
 },
 {
+  "id": 14,
   "prompt": "It looks a mess/It could be neater", 
 },
 {
+  "id": 15,
   "prompt": "It looks bad but the flavors are good", 
 },
 {
+  "id": 16,
   "prompt": "It looks plain/ underwhelming",
 },
 {
+  "id": 17,
   "prompt": "It tastes good but the textures are wrong",
 },
 {
+  "id": 18,
   "prompt": "I expected more",
 },
 {
+  "id": 19,
   "prompt": "Joke about Paul",
 },
 {
+  "id": 20,
   "prompt": "Overproved",
 },
 {
+  "id": 21,
   "prompt": "Paul questions someone's choice",
 },
 {
+  "id": 22,
   "prompt": "Somebody cooks something in a flavor one of the judges hates",
 },
 {
+  "id": 23,
   "prompt": "Raw",
 },
 {
+  "id": 24,
   "prompt": "Recipe from a relative",
 },
 {
+  "id": 25,
   "prompt": "Rubbery",
 },
 {
+  "id": 26,
   "prompt": "Soggy bottom",
 },
 {
+  "id": 27,
   "prompt": "Someone makes a mistake and starts over",
 },
 {
+  "id": 28,
   "prompt": "Stodgy", 
 },
 {
+  "id": 29,
   "prompt": "Textures are wrong",  
 },
 {
+  "id": 30,
   "prompt": "Two or more people with same idea", 
 },
 {
+  "id": 31,
   "prompt": "Underbaked",
 },
 {
+  "id": 32,
   "prompt": "Underproved",
 },
 {
+  "id": 33,
   "prompt": "Uneven distribution of filling",
 },
 {
+  "id": 34,
   "prompt": "Uneven layering"
 },
 ]
