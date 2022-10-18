a1ad35bb6a1c6c773e153b7844bb61aa83d4834d
lmadkins <louise.m.adkins@gmail.com>
2022-10-17 17:23:37 -0700

    Removed old ttf font files, removed instructions context as no longer needed

--- src/App.js
+++ src/App.js
@@ -6,23 +6,18 @@ import { useState } from 'react';
 import { Routes, Route} from "react-router-dom"
 import GamePage from './components/GamePage';
 import Home from './components/Home'
-import { InstructionsContext } from './context/InstructionsContext';
 import 'animate.css'
 
 const App = () => {
-// STATE TO PASS AS CONTEXT
-  const [openInstructions, setOpenInstructions] = useState(false)
-
+  
   return (  
     <>
-    <InstructionsContext.Provider value={{openInstructions, setOpenInstructions}}>
       <Routes>
         <Route>
           <Route path="/" element={<Home />}/>
           <Route path="/play" element={<GamePage />}/>
         </Route>
       </Routes>
-    </InstructionsContext.Provider>
     </>
   );
 };
